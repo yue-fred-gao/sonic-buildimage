9b4d018a60e29073224dd1e2de7547d6035b97a5
judsonwilson-nvidia <judsonw@nvidia.com>
2026-07-24 12:00:11 -0700

    Remove accidental creation of UEFI kvm image for non VS builds
    Remove accidental creation of UEFI kvm image for non VS builds

--- build_image.sh
+++ build_image.sh
@@ -178,7 +178,10 @@ elif [ "$IMAGE_TYPE" = "kvm" ]; then
     generate_onie_installer_image
     # Generate single asic KVM image
     generate_kvm_image 1
-    generate_kvm_image 1 UEFI
+    # Generate the UEFI image only for the VS platform.
+    if [ "$CONFIGURED_PLATFORM" = "vs" ]; then
+        generate_kvm_image 1 UEFI
+    fi
     if [ "$BUILD_MULTIASIC_KVM" == "y" ]; then
         # Generate 4-asic KVM image
         generate_kvm_image 4
